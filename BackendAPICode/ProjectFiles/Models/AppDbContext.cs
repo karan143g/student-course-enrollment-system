254c1d5de118f9fa02b16245ec444c0d7d61752a
karan143g <141416966+karan143g@users.noreply.github.com>
2025-05-21 23:15:08 +0530

    Update AppDbContext.cs

--- BackendAPICode/ProjectFiles/Models/AppDbContext.cs
+++ BackendAPICode/ProjectFiles/Models/AppDbContext.cs
@@ -4,12 +4,6 @@ namespace MovieRatingAppBackEnd.Models
 {
     public class AppDbContext: DbContext
     {
-        //public AppDbContext (DbContextOptions<AppDbContext>options) : base(options)
-        //{
-        //}
-        //public DbSet<Student> Student {  get; set; }
-      
-
         public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
         {
